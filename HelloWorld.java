36774986e98520f9e6861d77688529bc40eb27e0
unknown <dieste1254@gmail.com>
2023-01-22 22:34:53 -0300

    Realizando a tarefa da ebac

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main (String [] args){
+       String nome = args[0];
+        if(nome.equalsIgnoreCase("Diego")){
+            System.out.print("Ola " + nome);
+
+        }
+        else {
+
+            System.out.print("Esta errado, verifique e digite novamente");
+        }
+        }
+       
+    }
+
